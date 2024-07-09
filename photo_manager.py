a8a661d169e5429f77a503458d703a9925645540
Jose (Kaz) Casimiro Rios <jcasimir@redhat.com>
2024-07-09 14:16:02 -0500

    #3 Typo

--- photo_manager.py
+++ photo_manager.py
@@ -4,6 +4,7 @@ import random
 import requests
 import json
 import logging
+import time
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
